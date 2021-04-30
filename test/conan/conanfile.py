22e6cd7ab66327ce32678dce76cd05904e92bebc
Uilian Ries <uilianries@gmail.com>
2021-04-30 16:34:22 -0300

    Test on Xenial
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test/conan/conanfile.py
+++ test/conan/conanfile.py
@@ -1,16 +1,13 @@
 from conans import ConanFile, CMake
 
 class Pkg(ConanFile):
-    name = "foobar"
-    version = "0.1.0"
     settings = "os", "compiler", "arch", "build_type"
-    requires = "poco/1.9.4"
-    exports_sources = "CMakeLists.txt", "main.cpp"
+    requires = "zlib/1.2.11", "spdlog/1.8.5"
+    exports_sources = "CMakeLists.txt", "main.cpp", "main.c"
     generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
-        cmake.verbose = True
         cmake.configure()
         cmake.build()
 
